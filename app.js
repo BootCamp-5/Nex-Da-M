0279b5a66fec4aa73485b82be8b27c8cccc6ec13
임동빈 [bnitech] <bnitech@nexon.co.kr>
2019-01-14 20:34:40 +0900

    node js 기본 세팅

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res, next) => {
+    res.send('hello world!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at ${port}`);
+});
