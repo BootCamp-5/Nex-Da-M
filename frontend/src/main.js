ba90f194f9050525d67caadaeeee62192f771270
bnitech <bnitech88@gmail.com>
2019-01-21 02:16:56 +0900

    vue 새로운 세팅 npm install -g vue-cli vue init webpack frontend cd frontend npm install npm run dev

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,9 +4,6 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-import axios from 'axios'
-Vue.prototype.$http = axios
-
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
