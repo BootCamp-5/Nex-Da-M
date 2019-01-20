cb8080e0333fdd18d2a1e705eff47f4d0779f36b
bnitech <bnitech88@gmail.com>
2019-01-21 03:44:44 +0900

    express와 vue 연동 성공

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -9,6 +9,10 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+
+app.use(require('connect-history-api-fallback')());
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
