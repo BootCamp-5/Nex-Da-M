b55f1b84c5e3b7e2edd029873de37dc48630e27f
bnitech <bnitech88@gmail.com>
2019-01-21 03:39:59 +0900

    backend 구조 재구성

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,19 +1,41 @@
-const express = require('express');
-const app = express();
-const port = 8080;
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const index = require('./routes/index');
-const movies = require('./routes/movies');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
+var app = express();
 
-app.use('/index', index);
-app.use('/api/movies', movies);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-app.get('/', (req, res, next) => {
-    res.redirect('/index');
-    // res.send("ok!!!");
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.listen(port, () => {
-    console.log(`Server is running at ${port}`);
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
+
+module.exports = app;
